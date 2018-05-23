a6dbe6afd128713891ec8cb3084bbed1a74c5fd6
ValSbo <sborgia.valeria@gmail.com>
2018-05-23 17:12:42 +0200

    Update notizia.model.ts

--- model/notizia.model.ts
+++ model/notizia.model.ts
@@ -1,1 +1,11 @@
+import { InterpolationConfig } from "@angular/compiler";
 
+//import { Utente } from "./utente.model";
+
+export class Notizia {
+    id: number;
+    titolo: string;
+    testo: string;
+    data: string;
+    //pubblicatoDa: Utente;
+}
